1d30a05c0251bc733de9ad3fe89ed5863c904578
phsds <phsdscp@gmail.com>
2025-04-04 19:03:58 -0300

    Nova versão

--- installation.sh
+++
@@ -1,5 +0,0 @@
-#This file will help to install directly the PyPDF2 Library without typing the cammands.
-
-pip install --upgrade pip
-pip install PyPDF2 art termcolor
-pip install --upgrade PyPDF2 art termcolor
